daf0321ecf5d62cb4f5d964fabb50494d836b0fa
Robbie Kelly <robbiekelly@Robbies-MacBook-Air-2.local>
2025-03-10 15:33:31 -0400

    personal expense tracker update: create a file if there isn't one; set headers

--- PersonalExpenseTracker.py
+++ PersonalExpenseTracker.py
@@ -1,7 +1,16 @@
 #PERSONAL EXPENSE TRACKER
 import csv
+import os
 
-#Collect user input
+#create a way to check for the referenced file; if no file create one with specific headers (should only occur once)
+fileName = "expenseTracker.csv"
+
+if not os.path.exists(fileName):
+    with open('expenseTracker.csv', 'w', newline='') as f:
+        write = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        write.writerow("Date", "Expense", "Description", "Category")
+
+#Collect user expense input
 date = input("Expense Date? ")
 expense = input("How much was it? ")
 description = input("What store? ")
@@ -11,12 +20,12 @@ category = input("What categoy should this go in? ")
 NewExpense = [date, expense, description, category]
 
 #write new input to csv and display contents of csv
-with open('Expense_Tracker.csv', 'a', newline='') as f:
-    write = csv.writer(f, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+with open('expenseTracker.csv', 'a', newline='') as f:
+    write = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
     
     write.writerow(NewExpense)
 
-with open('Expense_Tracker.csv', 'r', newline='') as f:
+with open('expenseTracker.csv', 'r', newline='') as f:
     reader = csv.reader(f)
     for row in reader:
         print(', '.join(row))
