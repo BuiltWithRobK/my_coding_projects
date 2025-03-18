957c9f76383e13d7fd8bc63ffeda316b70274389
BuiltWithRobK <rkelly3@citadel.edu>
2025-03-18 08:07:21 -0400

    "update"

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 import re
 from datetime import datetime
-
 from flask import Flask
-
-from flask import 
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -16,7 +14,7 @@ def home():
 @app.route("/hello/<name>")
 def hello_there(name):
     now = datetime.now()
-    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+    formatted_now = now.strftime("%a, %d %b, %y at %X")
 
     # Filter the name argument to letters only using regular expressions. URL arguments
     # can contain arbitrary text, so we restrict to safe characters only.
@@ -29,3 +27,5 @@ def hello_there(name):
 
     content = "Hello there, " + clean_name + "! It's " + formatted_now
     return content
+
+
