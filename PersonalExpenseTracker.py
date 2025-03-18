cf64a7c8e9cc3fc345e9daa28a9dbc0f98839486
BuiltWithRobK <rkelly3@citadel.edu>
2025-03-18 14:17:41 -0400

    "update to personal expense tracker (adding classes and methods) and others"

--- PersonalExpenseTracker.py
+++ PersonalExpenseTracker.py
@@ -2,55 +2,76 @@
 import csv
 import os
 
-
-fileName = "expenseTracker.csv"
-headers = ["Date", "Expense", "Description", "Category"]
-
-#create a way to check for the referenced file; if no file create one with specific headers (should only occur once)
-if not os.path.exists(fileName):
-    with open('expenseTracker.csv', 'w', newline='') as f:
-        write = csv.writer(f)
-        write.writerow(headers)
-
-#Collect user expense input
-date = input("Expense Date? ")
-expense = input("How much was it? ")
-description = input("What store? ")
-
-#only certain categories are allowed, return error to reenter a possible category. error should include categories allowed.
-while True:
-    try:
-        category = input("What categoy should this go in? ")
-        if category.lower() == "gas":
-            category = "gas"
-            break
-        elif category.lower() == "groceries":
-            category = "groceries"
-            break 
-        elif category.lower() == "bills":
-            category = "groceries"
-            break 
-        elif category.lower() == "fun":
-            category = "groceries"
-            break
-        elif category.lower() == "fast food":
-            category = "groceries"
-            break
-        else:
-            print("Unrecognized category, try gas/groceries/bills/fun/fast food")
-    except ValueError:
-        print("Unrecognized category, try gas/groceries/bills/fun/fast food")
-
-#store user input as one value
-NewExpense = [date, expense, description, category]
-
-#write new input to csv and display contents of csv
-with open('expenseTracker.csv', 'a', newline='') as f:
-    write = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+class ExpenseTracker():
     
-    write.writerow(NewExpense)
+    def __init__(self, fileName = "expenseTracker.csv"): 
+        self.fileName = fileName
+        self.headers = ["Date", "Expense", "Description", "Category"]
 
-with open('expenseTracker.csv', 'r', newline='') as f:
-    reader = csv.reader(f)
-    for row in reader:
-        print(', '.join(row))
+        #create a way to check for the referenced file; if no file create one with specific headers (should only occur once)
+        if not os.path.exists(fileName):
+            with open('expenseTracker.csv', 'w', newline='') as f:
+                write = csv.writer(f)
+                write.writerow(self.headers)
+
+
+    def collect_user_expense(self):
+        #Collect user expense input
+        self.date = input("Expense Date? ")
+        self.expense = input("How much was it? ")
+        self.description = input("What store? ")
+        
+
+    def verify_category(self):
+        #only certain categories are allowed, return error to reenter a possible category. error should include categories allowed.
+        while True:
+            try:
+                category = input("What categoy should this go in? ")
+                if category.lower() == "gas":
+                    self.category = "gas"
+                    break
+                elif category.lower() == "groceries":
+                    self.category = "groceries"
+                    break 
+                elif category.lower() == "bills":
+                    self.category = "bills"
+                    break 
+                elif category.lower() == "fun":
+                    self.category = "fun"
+                    break
+                elif category.lower() == "fast food":
+                    self.category = "food"
+                    break
+                else:
+                    print("Unrecognized category, try gas/groceries/bills/fun/fast food")
+            except ValueError:
+                print("Unrecognized category, try gas/groceries/bills/fun/fast food")
+
+
+    def update_tracker(self):
+        #store user input as one value
+        self.NewExpense = [self.date, self.expense, self.description, self.category]    
+        #write new input to csv and display contents of csv
+        with open('expenseTracker.csv', 'a', newline='') as f:
+            write = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            
+            write.writerow(self.NewExpense)
+
+
+    def display_expense(self):
+        with open('expenseTracker.csv', 'r', newline='') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                print(', '.join(row))
+
+
+def main():
+    tracker = ExpenseTracker()
+    new_expense = tracker.collect_user_expense()
+    tracker.verify_category()
+    tracker.update_tracker()
+    tracker.display_expense()
+
+
+if __name__ == "__main__":
+    main()
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # My Personal Coding Projects
+
+
+#personal expense tracker:
+started with creating the basics for collecting user input. Used tutorials and other resources to learn to use csv and os libraries. once the basics of creating the the csv, collecting input, adding input, outputing values in csv were complete, I broke the program into a single class with it's own methods to clean and make the program more modular.
+
+next steps are to work on creating the expense tracker as a web app and make revisions/additions as necissary.
